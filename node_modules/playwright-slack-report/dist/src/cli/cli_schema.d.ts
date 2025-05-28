ec7e4aa0e8b2693f9a6161bbc355fa331bc96b98
Jeevan Reddy Maru <Jeevan.ReddyMaru@abcfitness.com>
2025-05-28 16:51:48 +0530

    Slack Changes

---
+++ node_modules/playwright-slack-report/dist/src/cli/cli_schema.d.ts
@@ -0,0 +1,134 @@
+import { z } from 'zod';
+import { LogLevel } from '@slack/web-api';
+export declare const ZodCliSchema: z.ZodObject<{
+    sendResults: z.ZodEnum<["always", "on-failure"]>;
+    sendUsingBot: z.ZodOptional<z.ZodObject<{
+        channels: z.ZodArray<z.ZodString, "atleastone">;
+    }, "strip", z.ZodTypeAny, {
+        channels?: [string, ...string[]];
+    }, {
+        channels?: [string, ...string[]];
+    }>>;
+    sendUsingWebhook: z.ZodOptional<z.ZodObject<{
+        webhookUrl: z.ZodString;
+    }, "strip", z.ZodTypeAny, {
+        webhookUrl?: string;
+    }, {
+        webhookUrl?: string;
+    }>>;
+    customLayout: z.ZodOptional<z.ZodObject<{
+        functionName: z.ZodString;
+        source: z.ZodString;
+    }, "strip", z.ZodTypeAny, {
+        source?: string;
+        functionName?: string;
+    }, {
+        source?: string;
+        functionName?: string;
+    }>>;
+    customLayoutAsync: z.ZodOptional<z.ZodObject<{
+        functionName: z.ZodString;
+        source: z.ZodString;
+    }, "strip", z.ZodTypeAny, {
+        source?: string;
+        functionName?: string;
+    }, {
+        source?: string;
+        functionName?: string;
+    }>>;
+    slackLogLevel: z.ZodNativeEnum<typeof LogLevel>;
+    maxNumberOfFailures: z.ZodDefault<z.ZodNumber>;
+    disableUnfurl: z.ZodDefault<z.ZodBoolean>;
+    showInThread: z.ZodDefault<z.ZodBoolean>;
+    proxy: z.ZodOptional<z.ZodString>;
+    meta: z.ZodOptional<z.ZodArray<z.ZodObject<{
+        key: z.ZodString;
+        value: z.ZodString;
+    }, "strip", z.ZodTypeAny, {
+        key?: string;
+        value?: string;
+    }, {
+        key?: string;
+        value?: string;
+    }>, "many">>;
+    sendCustomBlocksInThreadAfterIndex: z.ZodOptional<z.ZodNumber>;
+}, "strip", z.ZodTypeAny, {
+    meta?: {
+        key?: string;
+        value?: string;
+    }[];
+    proxy?: string;
+    maxNumberOfFailures?: number;
+    disableUnfurl?: boolean;
+    sendResults?: "always" | "on-failure";
+    sendUsingBot?: {
+        channels?: [string, ...string[]];
+    };
+    sendUsingWebhook?: {
+        webhookUrl?: string;
+    };
+    customLayout?: {
+        source?: string;
+        functionName?: string;
+    };
+    customLayoutAsync?: {
+        source?: string;
+        functionName?: string;
+    };
+    slackLogLevel?: LogLevel;
+    showInThread?: boolean;
+    sendCustomBlocksInThreadAfterIndex?: number;
+}, {
+    meta?: {
+        key?: string;
+        value?: string;
+    }[];
+    proxy?: string;
+    maxNumberOfFailures?: number;
+    disableUnfurl?: boolean;
+    sendResults?: "always" | "on-failure";
+    sendUsingBot?: {
+        channels?: [string, ...string[]];
+    };
+    sendUsingWebhook?: {
+        webhookUrl?: string;
+    };
+    customLayout?: {
+        source?: string;
+        functionName?: string;
+    };
+    customLayoutAsync?: {
+        source?: string;
+        functionName?: string;
+    };
+    slackLogLevel?: LogLevel;
+    showInThread?: boolean;
+    sendCustomBlocksInThreadAfterIndex?: number;
+}>;
+export interface ICliConfig {
+    sendResults: 'always' | 'on-failure';
+    sendUsingBot?: {
+        channels: string[];
+    };
+    sendUsingWebhook?: {
+        webhookUrl: string;
+    };
+    slackLogLevel: LogLevel;
+    customLayout?: {
+        functionName: string;
+        source: string;
+    };
+    customLayoutAsync?: {
+        functionName: string;
+        source: string;
+    };
+    maxNumberOfFailures: number;
+    disableUnfurl: boolean;
+    showInThread: boolean;
+    proxy?: string;
+    meta?: Array<{
+        key: string;
+        value: string;
+    }>;
+    sendCustomBlocksInThreadAfterIndex?: number;
+}
